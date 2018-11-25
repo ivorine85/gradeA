bf9f53650edd24f5653e2f8ef73fe8d92f475efe
Ivorine <ido@bu.edu>
2018-11-25 17:48:37 -0500

    login page update

--- Albert_Sze/Grade_A/GradeA/bin/GUI/gui/Login.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import javax.swing.*;
-
-public class Login {
-    private JButton btn_Login;
-    private JTextField usernameTextField;
-    private JTextField passwordTextField;
-}
