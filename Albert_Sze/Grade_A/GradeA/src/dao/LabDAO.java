8b8471402a489c42c84b88a4f28f8b3602d7c251
jingdian <jingdian@bu.edu>
2018-12-03 18:03:27 -0500

    finish all functions in add from scratch

--- Albert_Sze/Grade_A/GradeA/src/dao/LabDAO.java
+++ Albert_Sze/Grade_A/GradeA/src/dao/LabDAO.java
@@ -1,33 +1,31 @@
 package dao;
 
+import entity.Lab;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import entity.Course;
-
-public class CourseDAO {
+public class LabDAO {
     private Connection connection;
 
-    public boolean insert(Course c){
+    public boolean insert(Lab l){
         connection = Connector.getConnection();
-        String sql = "insert into Course (cname,startTime,endTime,startDate,endDate,weekDay,status) values (?,?,?,?,?,?,?)";
+        String sql = "insert into Lab (labname,startTime,endTime,weekDay,courseName) values (?,?,?,?,?)";
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
-            System.out.println(c.getClassTime()[0]+" "+c.getClassTime()[1]);
-            ps.setString(1,c.getCourseName());
-            ps.setTime(2,c.getClassTime()[0]);
-            ps.setTime(3,c.getClassTime()[1]);
-            ps.setDate(4,c.getClassDuration()[0]);
-            ps.setDate(5,c.getClassDuration()[1]);
+            ps.setString(1,l.getSection());
+            ps.setTime(2,l.getClasstime()[0]);
+            ps.setTime(3,l.getClasstime()[1]);
             String weekDay = "";
-            for(String s:c.getWeekDay()){
+            for(String s:l.getWeekday()){
                 weekDay+=s+",";
             }
-            ps.setString(6,weekDay);
-            ps.setInt(7,1);
+            ps.setString(4,weekDay);
+            ps.setString(5,l.getCourseName());
             ps.execute();
             ps.close();
             connection.close();
@@ -37,23 +35,27 @@ public class CourseDAO {
         return true;
     }
 
-    public int getIdByName(String courseName){
+    public List<Lab> findLabOfCourse(String courseName){
         connection = Connector.getConnection();
-        String sql = "select cid from course where cname =?";
-        int cid = -1;
+        String sql = "select * from lab where courseName =?";
+        List<Lab> rtn = new ArrayList<>();
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1,courseName);
             ResultSet res = ps.executeQuery();
-            res.next();
-            cid = res.getInt(1);
+            while(res.next()){
+                String[] days = res.getString(4).split(",");
+                Lab cur = new Lab(res.getString(1),res.getTime(2),res.getTime(3),days);
+                cur.setCourseName(res.getString(5));
+                rtn.add(cur);
+            }
             res.close();
             ps.close();
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return cid;
+        return rtn;
     }
 
 }
