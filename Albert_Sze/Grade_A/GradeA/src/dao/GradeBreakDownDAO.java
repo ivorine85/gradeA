cb8d7824d0ab023cbe2ab9ecd18edd1750c71c6e
jingdian <jingdian@bu.edu>
2018-12-03 22:07:03 -0500

    rewrite csv import

---
+++ Albert_Sze/Grade_A/GradeA/src/dao/GradeBreakDownDAO.java
@@ -0,0 +1,38 @@
+package dao;
+
+import entity.GradeBreakDown;
+import entity.Student;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class GradeBreakDownDAO {
+    Connection connection;
+
+    public boolean insert(GradeBreakDown g,Student s){
+        connection = Connector.getConnection();
+        String sql = "insert into gradebreakdown (cwname,coursename,typePercentage,percengate,type,weight,totalPoint,pointLost,sid) values (?,?,?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1,g.getCwName());
+            ps.setString(2,g.getCourseName());
+            ps.setFloat(3,g.getTypePercentage());
+            ps.setFloat(4,g.getPercentage());
+            ps.setString(5,g.getType());
+            ps.setInt(6,g.getWeight());
+            ps.setInt(7,g.getTotalPoint());
+            ps.setInt(8,g.getPointLost());
+            ps.setString(9,s.getSid());
+            ps.execute();
+            ps.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+
+}
