c5bbbab28ee5067ad108b8185fb412eb07f68ccf
jingdian <jingdian@bu.edu>
2018-12-05 13:09:41 -0500

    .

--- Albert_Sze/Grade_A/GradeA/src/gui/EditLab.java
+++ Albert_Sze/Grade_A/GradeA/src/gui/EditLab.java
@@ -1,246 +1,246 @@
-//package gui;
+package gui;
+
+import java.awt.*;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+
+public class EditLab {
+
+    private JFrame frame;
+    private JTextField textFieldLabSection;
+    private JTextField textFieldStartTime;
+    private JTextField textFieldEndTime;
+    private JLabel lblEditLab;
+
+    /**
+     * Launch the application.
+     */
+    //public static void main(String[] args) {
+    public static void ShowPage() {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    EditLab window = new EditLab();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public EditLab() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 1000, 489);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        // Label of window
+        JLabel lblEditLab = new JLabel("Edit Lab");
+        lblEditLab.setFont(new Font("Tahoma", Font.PLAIN, 36));
+        lblEditLab.setBounds(30, 20, 212, 44);
+        frame.getContentPane().add(lblEditLab);
+
+        JLabel lblLabSection = new JLabel("Lab Section");
+        lblLabSection.setBounds(40, 85, 100, 14);
+        frame.getContentPane().add(lblLabSection);
+
+        textFieldLabSection = new JTextField();
+        textFieldLabSection.setBounds(40, 110, 50, 20);
+        frame.getContentPane().add(textFieldLabSection);
+        textFieldLabSection.setColumns(10);
+
+        JLabel lblDate = new JLabel("Choose Date");
+        lblDate.setBounds(40, 145, 100, 14);
+        frame.getContentPane().add(lblDate);
+
+        JLabel lblMon = new JLabel("Mon");
+        lblMon.setBounds(40, 170, 46, 14);
+        frame.getContentPane().add(lblMon);
+
+        JRadioButton radioButtonMon = new JRadioButton("");
+        radioButtonMon.setBounds(43, 185, 109, 23);
+        frame.getContentPane().add(radioButtonMon);
+
+        JLabel lblTues = new JLabel("Tues");
+        lblTues.setBounds(75, 170, 46, 14);
+        frame.getContentPane().add(lblTues);
+
+        JRadioButton radioButtonTues = new JRadioButton("");
+        radioButtonTues.setBounds(78, 185, 109, 23);
+        frame.getContentPane().add(radioButtonTues);
+
+        JLabel lblWed = new JLabel("Wed");
+        lblWed.setBounds(115, 170, 46, 14);
+        frame.getContentPane().add(lblWed);
+
+        JRadioButton radioButtonWed = new JRadioButton("");
+        radioButtonWed.setBounds(118, 185, 109, 23);
+        frame.getContentPane().add(radioButtonWed);
+
+        JLabel lblThurs = new JLabel("Thurs");
+        lblThurs.setBounds(150, 170, 46, 14);
+        frame.getContentPane().add(lblThurs);
+
+        JRadioButton radioButtonThurs = new JRadioButton("");
+        radioButtonThurs.setBounds(153, 185, 109, 23);
+        frame.getContentPane().add(radioButtonThurs);
+
+        JLabel lblFri = new JLabel("Fri");
+        lblFri.setBounds(200, 170, 46, 14);
+        frame.getContentPane().add(lblFri);
+
+        JRadioButton radioButtonFri = new JRadioButton("");
+        radioButtonFri.setBounds(200, 185, 109, 23);
+        frame.getContentPane().add(radioButtonFri);
+
+        JLabel lblStartTime = new JLabel("Start Time");
+        lblStartTime.setBounds(40, 215, 100, 14);
+        frame.getContentPane().add(lblStartTime);
+
+        textFieldStartTime = new JTextField();
+        textFieldStartTime.setBounds(40, 240, 60, 20);
+        frame.getContentPane().add(textFieldStartTime);
+        textFieldStartTime.setColumns(10);
+
+        JComboBox<String> comboBoxStart = new JComboBox<String>();
+        comboBoxStart.addItem("AM");
+        comboBoxStart.addItem("PM");
+        comboBoxStart.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+            }
+        });
+        comboBoxStart.setBounds(100, 240, 70, 20);
+        frame.getContentPane().add(comboBoxStart);
+
+        JLabel lblEndTime = new JLabel("End Time");
+        lblEndTime.setBounds(180, 215, 100, 14);
+        frame.getContentPane().add(lblEndTime);
+
+        textFieldEndTime = new JTextField();
+        textFieldEndTime.setBounds(180, 240, 60, 20);
+        frame.getContentPane().add(textFieldEndTime);
+        textFieldEndTime.setColumns(10);
+
+        JComboBox<String> comboBoxEnd = new JComboBox<String>();
+        comboBoxEnd.addItem("AM");
+        comboBoxEnd.addItem("PM");
+        comboBoxEnd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+            }
+        });
+        comboBoxEnd.setBounds(240, 240, 70, 20);
+        frame.getContentPane().add(comboBoxEnd);
+
+        JLabel lblAssignTF = new JLabel("Assign TF");
+        lblAssignTF.setBounds(40, 270, 100, 14);
+        frame.getContentPane().add(lblAssignTF);
+
+        JComboBox<String> comboBoxTF = new JComboBox<String>();
+        comboBoxTF.addItem("Gavin Brown");
+        comboBoxTF.addItem("Nathan Canterbury");
+        comboBoxTF.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+            }
+        });
+        comboBoxTF.setBounds(40, 300, 200, 20);
+        frame.getContentPane().add(comboBoxTF);
+
+
+
+//        JLabel lblPhone = new JLabel("Phone #");
+//        lblPhone.setBounds(65, 88, 46, 14);
+//        frame.getContentPane().add(lblPhone);
 //
-//import java.awt.*;
+//        textField_1 = new JTextField();
+//        textField_1.setBounds(128, 85, 86, 20);
+//        frame.getContentPane().add(textField_1);
+//        textField_1.setColumns(10);
 //
-//import javax.swing.JFrame;
-//import javax.swing.JOptionPane;
-//import javax.swing.JButton;
-//import java.awt.event.ActionListener;
-//import java.awt.event.ActionEvent;
-//import javax.swing.JTextField;
-//import javax.swing.JLabel;
-//import javax.swing.JRadioButton;
-//import javax.swing.JComboBox;
+//        JLabel lblEmailId = new JLabel("Email Id");
+//        lblEmailId.setBounds(65, 135, 46, 14);
+//        frame.getContentPane().add(lblEmailId);
 //
-//public class EditLab {
+//        textField_2 = new JTextField();
+//        textField_2.setBounds(128, 132, 247, 17);
+//        frame.getContentPane().add(textField_2);
+//        textField_2.setColumns(10);
 //
-//    private JFrame frame;
-//    private JTextField textFieldLabSection;
-//    private JTextField textFieldStartTime;
-//    private JTextField textFieldEndTime;
-//    private JLabel lblEditLab;
+//        JLabel lblAddress = new JLabel("Address");
+//        lblAddress.setBounds(65, 182, 46, 14);
+//        frame.getContentPane().add(lblAddress);
 //
-//    /**
-//     * Launch the application.
-//     */
-//    //public static void main(String[] args) {
-//    public static void ShowPage() {
-//        EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                try {
-//                    EditLab window = new EditLab();
-//                    window.frame.setVisible(true);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//    }
-//
-//    /**
-//     * Create the application.
-//     */
-//    public EditLab() {
-//        initialize();
-//    }
-//
-//    /**
-//     * Initialize the contents of the frame.
-//     */
-//    private void initialize() {
-//        frame = new JFrame();
-//        frame.setBounds(100, 100, 1000, 489);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.getContentPane().setLayout(null);
-//
-//        // Label of window
-//        JLabel lblEditLab = new JLabel("Edit Lab");
-//        lblEditLab.setFont(new Font("Tahoma", Font.PLAIN, 36));
-//        lblEditLab.setBounds(30, 20, 212, 44);
-//        frame.getContentPane().add(lblEditLab);
-//
-//        JLabel lblLabSection = new JLabel("Lab Section");
-//        lblLabSection.setBounds(40, 85, 100, 14);
-//        frame.getContentPane().add(lblLabSection);
-//
-//        textFieldLabSection = new JTextField();
-//        textFieldLabSection.setBounds(40, 110, 50, 20);
-//        frame.getContentPane().add(textFieldLabSection);
-//        textFieldLabSection.setColumns(10);
-//
-//        JLabel lblDate = new JLabel("Choose Date");
-//        lblDate.setBounds(40, 145, 100, 14);
-//        frame.getContentPane().add(lblDate);
-//
-//        JLabel lblMon = new JLabel("Mon");
-//        lblMon.setBounds(40, 170, 46, 14);
-//        frame.getContentPane().add(lblMon);
-//
-//        JRadioButton radioButtonMon = new JRadioButton("");
-//        radioButtonMon.setBounds(43, 185, 109, 23);
-//        frame.getContentPane().add(radioButtonMon);
-//
-//        JLabel lblTues = new JLabel("Tues");
-//        lblTues.setBounds(75, 170, 46, 14);
-//        frame.getContentPane().add(lblTues);
-//
-//        JRadioButton radioButtonTues = new JRadioButton("");
-//        radioButtonTues.setBounds(78, 185, 109, 23);
-//        frame.getContentPane().add(radioButtonTues);
-//
-//        JLabel lblWed = new JLabel("Wed");
-//        lblWed.setBounds(115, 170, 46, 14);
-//        frame.getContentPane().add(lblWed);
-//
-//        JRadioButton radioButtonWed = new JRadioButton("");
-//        radioButtonWed.setBounds(118, 185, 109, 23);
-//        frame.getContentPane().add(radioButtonWed);
-//
-//        JLabel lblThurs = new JLabel("Thurs");
-//        lblThurs.setBounds(150, 170, 46, 14);
-//        frame.getContentPane().add(lblThurs);
-//
-//        JRadioButton radioButtonThurs = new JRadioButton("");
-//        radioButtonThurs.setBounds(153, 185, 109, 23);
-//        frame.getContentPane().add(radioButtonThurs);
-//
-//        JLabel lblFri = new JLabel("Fri");
-//        lblFri.setBounds(200, 170, 46, 14);
-//        frame.getContentPane().add(lblFri);
-//
-//        JRadioButton radioButtonFri = new JRadioButton("");
-//        radioButtonFri.setBounds(200, 185, 109, 23);
-//        frame.getContentPane().add(radioButtonFri);
-//
-//        JLabel lblStartTime = new JLabel("Start Time");
-//        lblStartTime.setBounds(40, 215, 100, 14);
-//        frame.getContentPane().add(lblStartTime);
-//
-//        textFieldStartTime = new JTextField();
-//        textFieldStartTime.setBounds(40, 240, 60, 20);
-//        frame.getContentPane().add(textFieldStartTime);
-//        textFieldStartTime.setColumns(10);
-//
-//        JComboBox<String> comboBoxStart = new JComboBox<String>();
-//        comboBoxStart.addItem("AM");
-//        comboBoxStart.addItem("PM");
-//        comboBoxStart.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent arg0) {
-//            }
-//        });
-//        comboBoxStart.setBounds(100, 240, 70, 20);
-//        frame.getContentPane().add(comboBoxStart);
-//
-//        JLabel lblEndTime = new JLabel("End Time");
-//        lblEndTime.setBounds(180, 215, 100, 14);
-//        frame.getContentPane().add(lblEndTime);
-//
-//        textFieldEndTime = new JTextField();
-//        textFieldEndTime.setBounds(180, 240, 60, 20);
-//        frame.getContentPane().add(textFieldEndTime);
-//        textFieldEndTime.setColumns(10);
-//
-//        JComboBox<String> comboBoxEnd = new JComboBox<String>();
-//        comboBoxEnd.addItem("AM");
-//        comboBoxEnd.addItem("PM");
-//        comboBoxEnd.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent arg0) {
-//            }
-//        });
-//        comboBoxEnd.setBounds(240, 240, 70, 20);
-//        frame.getContentPane().add(comboBoxEnd);
-//
-//        JLabel lblAssignTF = new JLabel("Assign TF");
-//        lblAssignTF.setBounds(40, 270, 100, 14);
-//        frame.getContentPane().add(lblAssignTF);
-//
-//        JComboBox<String> comboBoxTF = new JComboBox<String>();
-//        comboBoxTF.addItem("Gavin Brown");
-//        comboBoxTF.addItem("Nathan Canterbury");
-//        comboBoxTF.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent arg0) {
-//            }
-//        });
-//        comboBoxTF.setBounds(40, 300, 200, 20);
-//        frame.getContentPane().add(comboBoxTF);
-//
-//
-//
-////        JLabel lblPhone = new JLabel("Phone #");
-////        lblPhone.setBounds(65, 88, 46, 14);
-////        frame.getContentPane().add(lblPhone);
-////
-////        textField_1 = new JTextField();
-////        textField_1.setBounds(128, 85, 86, 20);
-////        frame.getContentPane().add(textField_1);
-////        textField_1.setColumns(10);
-////
-////        JLabel lblEmailId = new JLabel("Email Id");
-////        lblEmailId.setBounds(65, 135, 46, 14);
-////        frame.getContentPane().add(lblEmailId);
-////
-////        textField_2 = new JTextField();
-////        textField_2.setBounds(128, 132, 247, 17);
-////        frame.getContentPane().add(textField_2);
-////        textField_2.setColumns(10);
-////
-////        JLabel lblAddress = new JLabel("Address");
-////        lblAddress.setBounds(65, 182, 46, 14);
-////        frame.getContentPane().add(lblAddress);
-////
-////        JTextArea textArea_1 = new JTextArea();
-////        textArea_1.setBounds(126, 177, 212, 40);
-////        frame.getContentPane().add(textArea_1);
-//
-//
-//
-//        JButton btnClear = new JButton("Clear");
-//
-//        btnClear.setBounds(800, 410, 89, 23);
-//        frame.getContentPane().add(btnClear);
-//
-//
-//        JButton btnNext = new JButton("Finish");
-//
-////        btnCreate.setBackground(Color.BLUE);
-////        btnCreate.setForeground(Color.MAGENTA);
-//        btnNext.setBounds(900, 410, 89, 23);
-//        frame.getContentPane().add(btnNext);
-//
-//
-//        btnNext.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent arg0) {
-//                if(textFieldLabSection.getText().isEmpty()||(textFieldStartTime.getText().isEmpty())||(textFieldEndTime.getText().isEmpty())||((radioButtonTues.isSelected())&&(radioButtonMon.isSelected()))||(comboBoxStart.getSelectedItem().equals("Select")))
-//                    JOptionPane.showMessageDialog(null, "Data Missing");
-//                else{
-//                    JOptionPane.showMessageDialog(null, "Data Submitted");
-//                    LabPage returnLabPage = new LabPage();
-//                    returnLabPage.ShowPage();
-//                    frame.dispose();
-//                }
-//                LabPage returnLabPage = new LabPage();
-//                returnLabPage.ShowPage();
-//                frame.dispose();
-//
-//
-//            }
-//        });
-//
-//        btnClear.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent e) {
-//                textFieldStartTime.setText(null);
-//                textFieldEndTime.setText(null);
-//                textFieldLabSection.setText(null);
-//                radioButtonMon.setSelected(false);
-//                radioButtonTues.setSelected(false);
-//                radioButtonWed.setSelected(false);
-//                radioButtonThurs.setSelected(false);
-//                radioButtonFri.setSelected(false);
-//                comboBoxStart.setSelectedItem("Select");
-//
-//
-//            }
-//        });
-//
-//    }
-//}
+//        JTextArea textArea_1 = new JTextArea();
+//        textArea_1.setBounds(126, 177, 212, 40);
+//        frame.getContentPane().add(textArea_1);
+
+
+
+        JButton btnClear = new JButton("Clear");
+
+        btnClear.setBounds(800, 410, 89, 23);
+        frame.getContentPane().add(btnClear);
+
+
+        JButton btnNext = new JButton("Finish");
+
+//        btnCreate.setBackground(Color.BLUE);
+//        btnCreate.setForeground(Color.MAGENTA);
+        btnNext.setBounds(900, 410, 89, 23);
+        frame.getContentPane().add(btnNext);
+
+
+        btnNext.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                if(textFieldLabSection.getText().isEmpty()||(textFieldStartTime.getText().isEmpty())||(textFieldEndTime.getText().isEmpty())||((radioButtonTues.isSelected())&&(radioButtonMon.isSelected()))||(comboBoxStart.getSelectedItem().equals("Select")))
+                    JOptionPane.showMessageDialog(null, "Data Missing");
+                else{
+                    JOptionPane.showMessageDialog(null, "Data Submitted");
+                    LabPage returnLabPage = new LabPage();
+                    returnLabPage.ShowPage();
+                    frame.dispose();
+                }
+                LabPage returnLabPage = new LabPage();
+                returnLabPage.ShowPage();
+                frame.dispose();
+
+
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textFieldStartTime.setText(null);
+                textFieldEndTime.setText(null);
+                textFieldLabSection.setText(null);
+                radioButtonMon.setSelected(false);
+                radioButtonTues.setSelected(false);
+                radioButtonWed.setSelected(false);
+                radioButtonThurs.setSelected(false);
+                radioButtonFri.setSelected(false);
+                comboBoxStart.setSelectedItem("Select");
+
+
+            }
+        });
+
+    }
+}
