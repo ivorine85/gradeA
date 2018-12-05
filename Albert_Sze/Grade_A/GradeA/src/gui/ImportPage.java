481ff16c388dc0e69f6903e363d60f22a618e332
Albert Sze <asze@bu.edu>
2018-12-05 01:33:37 -0500

    updated CoursePage, EditGrades, ImportPage, LabPage, NewUser, and StudentProfile

--- Albert_Sze/Grade_A/GradeA/src/gui/ImportPage.java
+++ Albert_Sze/Grade_A/GradeA/src/gui/ImportPage.java
@@ -1,114 +1,116 @@
-//package gui;
-//
-//import java.awt.EventQueue;
-//
-//import javax.swing.JFrame;
-//import javax.swing.JLabel;
-//import javax.swing.JOptionPane;
-//
-//import java.awt.Font;
-//import javax.swing.JTextField;
-//
-//import Import.*;
-//import entity.*;
-//
-//import javax.swing.JButton;
-//import java.awt.event.ActionListener;
-//import java.io.File;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.awt.event.ActionEvent;
-//
-//public class ImportPage extends ImportCSV {
-//
-//	private JFrame frame;
-//	private JTextField textField;
-//
-//	/**
-//	 * Launch the application.
-//	 */
-//	//public static void main(String[] args) {
-//	public static void ShowPage() {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					ImportPage window = new ImportPage();
-//					window.frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
-//
-//	/**
-//	 * Create the application.
-//	 */
-//	public ImportPage() {
-//		initialize();
-//	}
-//
-//	/**
-//	 * Initialize the contents of the frame.
-//	 */
-//	private void initialize() {
-//		frame = new JFrame();
-//		frame.setBounds(100, 100, 450, 235);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.getContentPane().setLayout(null);
-//
-//		JLabel lblImportFile = new JLabel("Import File");
-//		lblImportFile.setFont(new Font("Tahoma", Font.PLAIN, 20));
-//		lblImportFile.setBounds(10, 11, 120, 31);
-//		frame.getContentPane().add(lblImportFile);
-//
-//		JLabel lblFilePath = new JLabel("File Path:");
-//		lblFilePath.setBounds(10, 64, 46, 14);
-//		frame.getContentPane().add(lblFilePath);
-//
-//		textField = new JTextField();
-//		textField.setBounds(10, 89, 403, 20);
-//		frame.getContentPane().add(textField);
-//		textField.setColumns(10);
-//
-//		JButton btnCancel = new JButton("Cancel");
-//		btnCancel.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				AddCourse addCoursePage = new AddCourse();
-//				addCoursePage.ShowPage();
-//				frame.dispose();
-//			}
-//		});
-//		btnCancel.setBounds(335, 163, 89, 23);
-//		frame.getContentPane().add(btnCancel);
-//
-//		JButton btnImport = new JButton("Import");
-//		btnImport.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				Path filePath = Paths.get(textField.getText());
-//				if(Files.notExists(filePath)) {
-//					JOptionPane.showMessageDialog(null,"File Path Does not exist.");
-//				}
-//				else if (filePath.toString().length()<5 || !filePath.toString().substring(filePath.toString().length()-3).equals("csv")) {
-//					JOptionPane.showMessageDialog(null,"File is not a csv");
-//				}
-//				else {
-//					try {
-//	                    Course newcourse = Import(filePath.toFile());
-//	                    //add to profile
-//	                    Dashboard dashboardPage = new Dashboard();
-//	                    dashboardPage.ShowPage();
-//	                    frame.disable();
-//	                }
-//	                catch (Exception ex) {
-//	                    System.out.println("CSV did not upload.");
-//	                }
-//				}
-//			}
-//		});
-//		btnImport.setBounds(236, 163, 89, 23);
-//		frame.getContentPane().add(btnImport);
-//	}
-//
-//}
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+
+import Import.*;
+import entity.*;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.awt.event.ActionEvent;
+
+public class ImportPage extends ImportCSV {
+
+	private JFrame frame;
+	private JTextField filePathTextField;
+
+	//public static void main(String[] args) {
+	public static void ShowPage() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ImportPage window = new ImportPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	public ImportPage() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		JLabel importFileTitle;
+		JLabel filePathTitle;
+		JButton cancelButton;
+		JButton importButton;
+		
+		/*********************************** Generate frame for Import File Page *******************************/
+		frame.setBounds(100, 100, 450, 235);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		/*********************************** Import File Title **************************************/
+		importFileTitle = new JLabel("Import File");
+		importFileTitle.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		importFileTitle.setBounds(10, 11, 120, 31);
+		frame.getContentPane().add(importFileTitle);
+
+		/*********************************** File path label ***********************************/
+		filePathTitle = new JLabel("File Path:");
+		filePathTitle.setBounds(10, 64, 65, 14);
+		frame.getContentPane().add(filePathTitle);
+
+		/*********************************** File path text field ***********************************/
+		filePathTextField = new JTextField();
+		filePathTextField.setBounds(10, 89, 403, 20);
+		frame.getContentPane().add(filePathTextField);
+		filePathTextField.setColumns(10);
+
+		/*********************************** Create Cancel button ************************************/
+		cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AddCourse addCoursePage = new AddCourse();
+				addCoursePage.ShowPage();
+				frame.dispose();
+			}
+		});
+		cancelButton.setBounds(335, 163, 89, 23);
+		frame.getContentPane().add(cancelButton);
+
+		/*********************************** Create Import button ************************************/
+		importButton = new JButton("Import");
+		importButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Path filePath = Paths.get(filePathTextField.getText());
+				if(Files.notExists(filePath)) {
+					JOptionPane.showMessageDialog(null,"File Path Does not exist.");
+				}
+				else if (filePath.toString().length()<5 || !filePath.toString().substring(filePath.toString().length()-3).equals("csv")) {
+					JOptionPane.showMessageDialog(null,"File is not a csv");
+				}
+				else {
+					try {
+	                    Course newcourse = Import(filePath.toFile());
+	                    //add to profile
+	                    Dashboard dashboardPage = new Dashboard();
+	                    dashboardPage.ShowPage();
+	                    frame.disable();
+	                }
+	                catch (Exception ex) {
+	                    System.out.println("CSV did not upload.");
+	                }
+				}
+			}
+		});
+		importButton.setBounds(236, 163, 89, 23);
+		frame.getContentPane().add(importButton);
+	}
+}
