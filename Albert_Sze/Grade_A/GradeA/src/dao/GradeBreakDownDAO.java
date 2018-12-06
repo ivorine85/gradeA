91307c86f4e0e752cad789e2b90425911e20d33e
jingdian <jingdian@bu.edu>
2018-12-06 17:17:47 -0500

    Merge branch 'master' of https://github.com/ivorine85/gradeA
    # Conflicts:
    #	Albert_Sze/Grade_A/GradeA/src/gui/AddStudents.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/CoursePage.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/EditGrades.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/ImportPage.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/LabPage.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/StudentProfile.java

--- Albert_Sze/Grade_A/GradeA/src/dao/GradeBreakDownDAO.java
+++ Albert_Sze/Grade_A/GradeA/src/dao/GradeBreakDownDAO.java
@@ -4,11 +4,14 @@ import entity.GradeBreakDown;
 import entity.Student;
 
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GradeBreakDownDAO {
@@ -16,7 +19,7 @@ public class GradeBreakDownDAO {
 
     public boolean insert(GradeBreakDown g,Student s){
         connection = Connector.getConnection();
-        String sql = "insert into gradebreakdown (cwname,coursename,typePercentage,percengate,type,weight,totalPoint,pointLost,sid) values (?,?,?,?,?,?,?,?,?)";
+        String sql = "insert into gradebreakdown (cwname,coursename,typePercentage,percentage,type,weight,totalPoint,pointLost,sid) values (?,?,?,?,?,?,?,?,?)";
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1,g.getCwName());
@@ -41,14 +44,14 @@ public class GradeBreakDownDAO {
     public Map<String,Double[]> getPerformance(String courseName){
         connection = Connector.getConnection();
         Map<String,Double[]> r = new HashMap<>();
-        String sql = "select cwname,avg(pointlost),avg(totalPoint) from gradeBreakDown where coursename = ? group by  cwname;";
+        String sql = "select cwname,avg(pointlost),avg(totalPoint),avg(weight) from gradeBreakDown where coursename = ? group by  cwname;";
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1,courseName);
             ResultSet res = ps.executeQuery();
             while(res.next()){
                 String cwname = res.getString(1);
-                double avgPointLost = res.getDouble(2);
+                double avgPointLost = res.getDouble(2)-res.getDouble(4);
                 double total = res.getDouble(3);
                 r.put(cwname,new Double[]{avgPointLost,total});
                 System.out.println(cwname+":"+ avgPointLost+"."+total);
@@ -60,6 +63,32 @@ public class GradeBreakDownDAO {
             e.printStackTrace();
         }
         return r;
+    }
+
+    public List<GradeBreakDown> getGradeByStudent(String sid,String cname){
+        connection = Connector.getConnection();
+        List<GradeBreakDown> rtn = new ArrayList<>();
+        String sql = "select cwname,coursename,typePercentage,percentage,type,weight,totalpoint,pointLost from gradeBreakDown where coursename = ? && sid = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1,cname);
+            ps.setString(2,sid);
+            ResultSet res = ps.executeQuery();
+            while(res.next()){
+                GradeBreakDown cur = new GradeBreakDown(res.getString(1),
+                        res.getString(2),
+                        res.getFloat(3),
+                        res.getFloat(4),
+                        res.getString(5),
+                        res.getInt(6),
+                        res.getInt(7),
+                        res.getInt(8));
+                rtn.add(cur);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rtn;
     }
 
 }
