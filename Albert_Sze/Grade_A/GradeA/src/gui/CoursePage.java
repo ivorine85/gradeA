16c9169a5853a1943248a8dbf69d4de60f7f8b50
jingdian <jingdian@bu.edu>
2018-12-06 14:18:31 -0500

    Merge branch 'master' of https://github.com/ivorine85/gradeA
    # Conflicts:
    #	Albert_Sze/Grade_A/GradeA/src/gui/AddStudents.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/CoursePage.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/EditGrades.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/ImportPage.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/LabPage.java
    #	Albert_Sze/Grade_A/GradeA/src/gui/StudentProfile.java

--- Albert_Sze/Grade_A/GradeA/src/gui/CoursePage.java
+++ Albert_Sze/Grade_A/GradeA/src/gui/CoursePage.java
@@ -38,8 +38,8 @@ public class CoursePage {
 	private static Course currentCourse;
 
 
-	//public static void ShowPage() {
-	public static void main(String[] args) {
+	public static void ShowPage() {
+	//public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
