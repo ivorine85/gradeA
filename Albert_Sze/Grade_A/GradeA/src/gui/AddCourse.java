1015764d93b67d274a1f3485f5baf3a00102d272
Ivorine <ido@bu.edu>
2018-12-10 13:36:34 -0500

    minor fixes- added back buttons

--- Albert_Sze/Grade_A/GradeA/src/gui/AddCourse.java
+++ Albert_Sze/Grade_A/GradeA/src/gui/AddCourse.java
@@ -55,7 +55,7 @@ public class AddCourse {
         lblAddCourse.setBounds(30, 20, 212, 44);
         frame.getContentPane().add(lblAddCourse);
 
-        JButton btnUpload = new JButton("Upload Excel File");
+        JButton btnUpload = new JButton("Upload CSV File");
 
         btnUpload.setBounds(330, 150, 200, 23);
         frame.getContentPane().add(btnUpload);
@@ -73,7 +73,7 @@ public class AddCourse {
             public void actionPerformed(ActionEvent arg0) {
 
                 CourseInfo courseInfoPage = new CourseInfo();
-                //courseInfoPage.ShowPage();
+                courseInfoPage.ShowPage();
                 frame.dispose();
 
                 //CoursePage changePage = new CoursePage();
@@ -92,91 +92,19 @@ public class AddCourse {
             }
         });
 
-        //        loginButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                /*
-//                String password = String.valueOf(input_pass.getPassword());
-//                String username = input_uname.getText();
-//                UserDAO userDAO = new UserDAO();
-//                String psw = userDAO.getPsw(username);
-//                if(psw.equals("No such user!")){
-//                    System.out.println("can't find this username!");
-//                    JOptionPane.showMessageDialog(null, "User does not exist.");
-//                }
-//                else if(psw.equals(password)){
-//                    System.out.println("Match!");
-//                    Dashboard dashboardPage = new Dashboard();
-//                    dashboardPage.ShowPage();
-//                    frame.dispose();
-//                }
-//                else{
-//                    JOptionPane.showMessageDialog(null, "Username or Password incorrect");
-//                    System.out.println("Wrong!");
-//                }
-//                */
-//                Dashboard dashboardPage = new Dashboard();
-//                dashboardPage.ShowPage();
-//                frame.dispose();
-//            }
-//        });
-//        createUserButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//                //go to new user page, page not created yet
-//                NewUser newUserPage = new NewUser();
-//                newUserPage.ShowPage();
-//                frame.dispose();
-//            }
-//        });
-
+        //Back Button
+        JButton btnBack = new JButton("Back");
+        btnBack.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Dashboard dashboardPage = new Dashboard();
+                dashboardPage.ShowPage();
+                frame.dispose();
+            }
+        });
+        btnBack.setBounds(30, 400, 89, 23);
+        frame.getContentPane().add(btnBack);
 
 
     }
 }
 
-
-//package gui;
-//
-//import javax.swing.*;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import Import.*;
-
-//public class AddCourse {
-//    private JButton btnUpload;
-//    private JButton btnCreate;
-//    private JLabel lblAddCourse;
-//    private JPanel panelAddCourse;
-//    private static JFrame frame;
-//
-//    public AddCourse() {
-//        btnUpload.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                //ImportPage importPageNext = new ImportPage();
-//                //importPageNext.ShowPage();
-//                frame.dispose();
-//            }
-//        });
-//        btnCreate.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                String[] args = new String[0];
-//                CourseInfo courseInfoPage = new CourseInfo();
-//                CourseInfo.ShowPage();
-//                frame.dispose();
-//            }
-//        });
-//    }
-//
-//    public static void main(String[] args) {
-//    //public static void ShowPage() {
-//        frame = new JFrame("Add Course");
-//        frame.setContentPane(new AddCourse().panelAddCourse);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.pack();
-//        frame.setVisible(true);
-//    }
-//}
